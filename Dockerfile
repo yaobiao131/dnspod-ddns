c2e46171486905091694aa44b5b7b646605d334d
姚彪 <1315508912@qq.com>
2020-09-06 23:52:23 +0800

    fix:修复拼写错误

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
-FROM python:3-alpine
+FROM python:3.8-alpine
 
-MAINTAINER strahe <u@strahe.com>
+MAINTAINER zecbd <u@strahe.com>
 
 WORKDIR /app
 
 ADD . /app
 
-RUN "pip install -r requirements.txt"
+RUN apk add gcc linux-headers libc-dev musl-dev libffi-dev openssl-dev
+
+RUN pip3 install -r requirements.txt
 
 ENTRYPOINT ["python", "ddns.py"]
--- telegramBot.py
+++ telegramBot.py
@@ -1,15 +1,13 @@
 import logging
 from telegram import Bot
-from telegram.utils import request
-from config import read_config, save_config, check_config, cfg
+from config import read_config, cfg
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 
 def send_message(message):
-    request1 = request.Request(1, 'http://127.0.0.1:7890')
-    bot = Bot(token=cfg['telegram_bot_api_token'], request=request1)
+    bot = Bot(token=cfg['telegram_bot_api_token'])
     bot.send_message(cfg['telegram_chat_id'], message)
 
 
